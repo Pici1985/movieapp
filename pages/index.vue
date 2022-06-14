54ae98829db02dbefa1f38446b8b9dfe7d139285
Balazs <balazs@frog.net>
2022-06-14 09:20:59 +0100

    Updated ESlint

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="home">
-    
+    <Hero />     
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'IndexPage',
+  data(){
+    return {
+      movies: [],
+    }
+  },
+  async fetch(){
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=b810837502842dbe5c8955ffa74cf870&language=en-US&page=1')
+      const result = await data;
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+      // console.log(result.data.results)
+      // console.log(this.movies)
+    }
+  }  
 }
 </script>
