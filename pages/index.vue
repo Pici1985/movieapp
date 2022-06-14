246e0d049e1c4795212b8a9823d2ed9cc0c2281e
Balazs <balazs@frog.net>
2022-06-14 15:28:12 +0100

    Search Movie
    search movie function works

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,51 @@
 
     <!-- Movies -->
     <div class="container movies">
-      <div id="movie-grid" class="movies-grid">
+      <!-- searched movies -->
+      <div 
+        v-if="searchInput !== ''"
+        id="movie-grid" 
+        class="movies-grid"
+      >
+        <div 
+          v-for="(movie, index) in searchedMovies" :key="index"
+          class="movie" 
+        >
+          <div class="movie-img">
+            <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" alt="">
+            <p class="review">{{ movie.vote_average}}</p>
+            <p class="overview">{{ movie.overview }}</p>
+          </div>
+          <div class="info">
+            <p class="title"> 
+              {{ movie.title.slice(0,25) }} 
+              <span v-if="movie.title.lenth > 25">...</span> 
+            </p>
+            <p class="release">
+              Released: 
+              {{
+                new Date(movie.release_date).toLocaleString('en-us', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric'
+                })
+              }}
+            </p>
+            <NuxtLink 
+              :to="{ name: 'movies-movieid', params: {movieid: movie.id}}"
+              class="button button-light" 
+            >
+              Get More Info
+            </NuxtLink>  
+          </div>  
+        </div>  
+      </div>
+      <!-- Now streaming movies -->
+      <div 
+        v-else
+        id="movie-grid" 
+        class="movies-grid"
+      >
         <div 
           v-for="(movie, index) in movies" :key="index"
           class="movie" 
@@ -49,8 +93,10 @@
             </NuxtLink>  
           </div>  
         </div>  
-      </div>    
-    </div> 
+      </div>       
+    </div>
+    
+     
 
   </div>
 </template>
@@ -88,7 +134,7 @@ export default {
       result.data.results.forEach((movie) => {
         this.searchedMovies.push(movie)
       }) 
-      console.log(this.searchedMovies)
+      // console.log(this.searchedMovies)
     }
   },
 }
